2fa28440a29a38cf9c9ad6f93841b6cc072cf556
ds0226 <davidsonsolomon26@gmail.com>
2025-11-22 09:12:31 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ const fs = require('fs');
 
 
 // --- MongoDB and Mongoose Integration ---
-const uri = process.env.MONGO_URI || "mongodb+srv://david26:davien1130@ebab.w90ig5m.mongodb.net/?appName=EBAB";
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://david26:davien1130@ebab.w90ig5m.mongodb.net/?appName=EBAB";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 mongoose.connect(uri)
   .then(() => console.log('Successfully connected to MongoDB Atlas!'))
